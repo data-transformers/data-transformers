e5e6036f0ef0d8b1e224cb38d16f5735b580f801
Bayzed Ullah <bayzgraphics@gmail.com>
2023-08-31 15:06:44 +0000

    src and stg setup

---
+++ models/staging/stg_ps_4_video_game_sales.sql
@@ -0,0 +1,11 @@
+select
+    _row as id,  
+    game AS name, 
+    publisher, 
+    genre, 
+    year, 
+    north_america, 
+    europe, japan, 
+    rest_of_world,
+    global 
+from {{ source("video_game_sales", "PS_4_GAME_SALES")}}
