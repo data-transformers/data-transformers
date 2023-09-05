44890006fc0f1faec21d9a523af5ca7082809fbc
Bayzed Ullah <bayzgraphics@gmail.com>
2023-09-05 03:52:41 +0000

    question 5 updated

--- models/marts/core/dim_successfull_pub.sql
+++ models/marts/core/dim_successfull_pub.sql
@@ -1,4 +1,3 @@
-
 {{ config(
     materialized="table",
     schema="dbt_muddin"
@@ -19,15 +18,25 @@ region_sum as (
     SUM(rest_of_world) as row_sales
     FROM int_game_sales
     GROUP BY genre, publisher
-),
+), 
 
-dim_successful_pub as (
+genre_sales_rank as (
     SELECT
     genre,
     publisher,
-    na_sales + eu_sales + jap_sales + row_sales as total_sales
+    na_sales + eu_sales + jap_sales + row_sales as total_sales,
+    RANK() OVER (PARTITION BY GENRE ORDER BY NA_SALES + EU_SALES + JAP_SALES + ROW_SALES DESC) AS sales_rank
     FROM region_sum
+),
+
+dim_successful_pub as (
+SELECT
+    genre,
+    publisher,
+    total_sales
+FROM genre_sales_rank
+WHERE sales_rank = 1
 )
 
-SELECT *
+SELECT genre, publisher
 FROM dim_successful_pub
