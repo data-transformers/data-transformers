83cd1eca89115a3c1f56e744c322adc8a47bcc6c
Bayzed Ullah <bayzgraphics@gmail.com>
2023-09-06 18:51:22 +0000

    working on question 6

---
+++ models/marts/core/pop_games_regional.sql
@@ -0,0 +1,41 @@
+{{ config(
+    materialized="table",
+    schema="dbt_muddin"
+) }}
+
+with int_game_sales as ( 
+    SELECT *
+    FROM {{ ref("int_game_sales") }}
+),
+
+region_sum as (
+    SELECT 
+    name,
+    SUM(north_america) as na_sales,
+    SUM(europe) as eu_sales,
+    SUM(japan) as jap_sales,
+    SUM(rest_of_world) as row_sales
+    FROM int_game_sales
+    GROUP BY name
+)
+
+-- genre_sales_rank as (
+--     SELECT
+--     genre,
+--     publisher,
+--     na_sales + eu_sales + jap_sales + row_sales as total_sales,
+--     RANK() OVER (PARTITION BY GENRE ORDER BY NA_SALES + EU_SALES + JAP_SALES + ROW_SALES DESC) AS sales_rank
+--     FROM region_sum
+-- )
+
+-- dim_successful_pub as (
+-- SELECT
+--     genre,
+--     publisher,
+--     total_sales
+-- FROM genre_sales_rank
+-- WHERE sales_rank = 1
+-- )
+
+SELECT *
+FROM region_sum
